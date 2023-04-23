351f39fdea055645aa7304ec8cc939468da67012
Tauseef Shah <tauseefsshah@gmail.com>
2023-04-23 10:55:04 +0530

    ADD - Inertiajs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import { Glob } from "glob";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        vue(),
     ],
+    resolve: (name) => {
+        const pages = Glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
+    },
 });
