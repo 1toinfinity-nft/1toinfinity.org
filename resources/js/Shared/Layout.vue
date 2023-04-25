ab434c965b864e9aaea3ebc8201d8fbe68d91f18
Tauseef Shah <tauseefsshah@gmail.com>
2023-04-25 20:48:24 +0530

    CREATE - Mobile navigation

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -7,7 +7,7 @@
                         1-to-Infinity
                     </h1>
                 </div>
-                <nav id="navigation">
+                <nav id="desktop-navigation" class="hidden md:block">
                     <ol class="flex gap-8 text-sm font-bold">
                         <Link
                             :href="navigationItem.link"
@@ -21,7 +21,56 @@
                         </Link>
                     </ol>
                 </nav>
-                <div id="call-to-action">
+                <nav
+                    :class="{
+                        'absolute inset-0 bg-primary-dark border-y-2 p-4 w-full h-screen z-50 md:hidden': true,
+                        hidden: !isMobileSidebarOpen,
+                    }"
+                >
+                    <div
+                        id="mobile-navigation-toggle"
+                        class="font-bold text-lg text-right"
+                        @click="toggleMobileSidebar()"
+                    >
+                        X
+                    </div>
+                    <ul class="list-none flex flex-col gap-12">
+                        <li
+                            class="flex items-center text-white text-2xl font-bold"
+                            v-for="navigationItem in navigationItems"
+                        >
+                            <a :href="navigationItem.link">
+                                {{ navigationItem.label }}
+                            </a>
+                        </li>
+                        <li
+                            class="flex items-center text-white text-2xl font-bold"
+                        >
+                            <a
+                                href="https://opensea.io/collection/1toinfinity-nft"
+                                class="flex items-center gap-2"
+                                target="_blank"
+                            >
+                                <span>OpenSea</span>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="3"
+                                    stroke="currentColor"
+                                    class="w-4 h-4"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+                                    />
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div id="desktop-call-to-action" class="hidden md:block">
                     <a
                         class="bg-gray-500 hover:bg-gray-600 px-4 py-2 rounded-lg font-bold flex items-center gap-2"
                         href="https://opensea.io/collection/1toinfinity-nft"
@@ -43,6 +92,26 @@
                             />
                         </svg>
                     </a>
+                </div>
+                <div
+                    id="mobile-sidebar-toggle"
+                    class="block md:hidden"
+                    @click="toggleMobileSidebar()"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                        />
+                    </svg>
                 </div>
             </div>
         </div>
@@ -89,6 +158,7 @@
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
 
 const navigationItems = [
     {
@@ -117,4 +187,10 @@ const navigationItems = [
         isActive: route().current("front.blog"),
     },
 ];
+
+const isMobileSidebarOpen = ref(false);
+
+const toggleMobileSidebar = () => {
+    isMobileSidebarOpen.value = !isMobileSidebarOpen.value;
+};
 </script>
