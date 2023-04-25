e63d12eb5ecc07559b368601b9e45fa4627fa228
Tauseef Shah <tauseefsshah@gmail.com>
2023-04-25 18:03:46 +0530

    CREATE - /open route and view

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ Route::get('/', fn () => inertia('Home'))->name('front.home');
 Route::get('/about', fn () => inertia('About'))->name('front.about');
 Route::get('/blog', fn () => inertia('Blog'))->name('front.blog');
 Route::get('/collection', fn () => inertia('Collection'))->name('front.collection');
+Route::get('/open', fn () => inertia('Open'))->name('front.open');
 Route::get('/roadmap', fn () => inertia('Roadmap'))->name('front.roadmap');
