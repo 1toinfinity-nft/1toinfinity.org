39d49e973224404c584568abd815f00c72179170
Tauseef Shah <tauseefsshah@gmail.com>
2023-04-30 15:23:17 +0530

    CREATE - Roadmap. WIP

--- resources/js/Pages/Roadmap.vue
+++ resources/js/Pages/Roadmap.vue
@@ -1,8 +1,64 @@
 <template>
     <Layout>
-        <section id="roadmap" class="py-12 md:py-40">
+        <section id="roadmap" class="py-12">
             <div class="container max-w-5xl mx-auto">
-                <h1 class="text-4xl text-center">Roadmap</h1>
+                <div class="flex flex-col gap-12 max-w-lg m-auto">
+                    <h1 class="text-4xl text-center">Roadmap</h1>
+                    <div id="roadmap-points">
+                        <ul class="list-disc text-justify">
+                            <li>
+                                <b>01 May 2023:</b> Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit. Sequi vero earum
+                                nemo, iste assumenda in rerum obcaecati a
+                                reiciendis illum.
+                            </li>
+                            <li>
+                                <b>01 May 2023:</b> Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit. Sequi vero earum
+                                nemo, iste assumenda in rerum obcaecati a
+                                reiciendis illum.
+                            </li>
+                            <li>
+                                <b>01 May 2023:</b> Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit. Sequi vero earum
+                                nemo, iste assumenda in rerum obcaecati a
+                                reiciendis illum.
+                            </li>
+                            <li>
+                                <b>01 May 2023:</b> Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit. Sequi vero earum
+                                nemo, iste assumenda in rerum obcaecati a
+                                reiciendis illum.
+                            </li>
+                            <li>
+                                <b>01 May 2023:</b> Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit. Sequi vero earum
+                                nemo, iste assumenda in rerum obcaecati a
+                                reiciendis illum.
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="text-xs">
+                        <b>Notes</b>
+                        <ul class="list-decimal text-justify">
+                            <li>
+                                We also have a private roadmap. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, vitae.*
+                            </li>
+                            <li>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse animi id harum cumque minus tempora.
+                            </li>
+                            <li>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse animi id harum cumque minus tempora.
+                            </li>
+                            <li>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse animi id harum cumque minus tempora.
+                            </li>
+                            <li>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse animi id harum cumque minus tempora.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </section>
     </Layout>
