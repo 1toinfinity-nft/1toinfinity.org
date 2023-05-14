91e8c129ac618cdf49df780fdf2655cb7a2867a9
Tauseef Shah <tauseefsshah@gmail.com>
2023-05-14 17:58:15 +0530

    UPDATE - Collection

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -156,14 +156,18 @@ const collection = [
         number: "1",
         words: "one",
         preview: "/build/collection/one.jpg",
-        opensea: "https:/opensea.io/assets/ethereum/asset-address-will-be-here",
+        opensea:
+            "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/75954781589527691372109163814478728017512661675042353574069749889233477173249",
+        selling_price: "0.0001ETH",
         published_at: "2023-05-07",
     },
     {
         number: "2",
         words: "two",
         preview: "/build/collection/two.jpg",
-        opensea: "https:/opensea.io/assets/ethereum/asset-address-will-be-here",
+        opensea:
+            "https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/75954781589527691372109163814478728017512661675042353574069749890332988801025",
+        selling_price: NaN,
         published_at: "2023-05-14",
     },
 ];
